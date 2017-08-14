c70e39457af35e10c49184eadc95e4846e714726
masarath mohammed <masarathmohammed@DSWINC.COM>
2017-08-14 16:38:54 -0400

    updates

--- src/main/resources/static/scripts/controller/home_ctrl.js
+++ src/main/resources/static/scripts/controller/home_ctrl.js
@@ -6,14 +6,19 @@ angular.module('CouponMaster.home', ['bw.paging'])
         
     $scope.pageSize = 100;
     $scope.dataLoading = true;
+    $scope.pagination = {
+            current: 1
+        };
+    $scope.currentPage=1;
     // Data model binding
     $scope.loadData = function (pNumber, pSize) {
         util.callRequest('/allDeals', "GET", {pageNumber: pNumber, pageSize: pSize}).then(function (data) {
             console.log(data);
         	$scope.deals = data;
-            $scope.totalPage = data.total_pages;
+        	$scope.page= data.number;
+        	$scope.totalPage = data.total_pages;
             $scope.total = data.total_pages;
-            $scope.pageSize = data.total_elements;
+            $scope.totalDeals=data.total_elements;
             $scope.currentPage= data.number;
             $scope.dataLoading = false;
         });
@@ -27,17 +32,17 @@ angular.module('CouponMaster.home', ['bw.paging'])
     $scope.loadData(0, $scope.pageSize);
     
     // angular event listener for event of search box directive
-    $scope.$on( 'searchResult', function( event, data ) {
+   /* $scope.$on( 'searchResult', function( event, data ) {
         console.debug( data );
         // binding search data into product list
-        $scope.products = response.data;
+        $scope.deals = response.data;
         $scope.totalPage = data.total_records;
     });
     
     $scope.$on("priceRangeResult", function (event, data) {
         // user finished sliding a handle
         console.debug( data );
-    });
+    });*/
     
 }]);
 
